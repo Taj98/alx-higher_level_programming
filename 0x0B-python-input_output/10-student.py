e838a8dc9235ba371775412add4f7ce535586819
Taj98 <chillzy98@gmail.com>
2022-10-27 14:51:55 +0200

    Bug fixes

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
+''' class Student that defines a student
+'''
 
 
 class Student:
-    """Type class of a student"""
+    '''module class student
+    '''
 
     def __init__(self, first_name, last_name, age):
-
+        '''method __init__
+        '''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
-    def to_json(self):
-
-        return self.__dict__
--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-"""
-function that inserts a line of text to a file, after each line containing a specific string
-"""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    '''module Search and update
-    '''
-    with open(filename, 'r+') as f:
-        lines = f.readlines()
-        i = 0
-        for line in lines:
-            if line.find(search_string) is not -1:
-                lines.insert(i + 1, new_string)
-            i += 1
-        f.seek(0)
-        f.write("".join(lines))
+
+    txt = ""
+    with open(filename) as r:
+        for line in r:
+            txt += line
+            if search_string in line:
+                txt += new_string
+    with open(filename, "w") as w:
+        w.write(txt)
